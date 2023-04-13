9eaa1183fbd7180f75f010ae789c4acb87c2b365
Ignas Galvelis <igalvelis@gmail.com>
2023-04-13 20:40:24 +0100

    Style and add more function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,22 @@ const Evaluate: React.FC = () => {
   const [solution, setSolution] = useState('');
   const [happyEnding, setHappyEnding] = useState('');
   const [evaluation, setEvaluation] = useState('');
+  const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
   const handleChange = (
     setter: React.Dispatch<React.SetStateAction<string>>
   ) => (e: ChangeEvent<HTMLTextAreaElement>) => {
     setter(e.target.value);
+    setError('');
   };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    if (!problem || !solution || !happyEnding) {
+      setError('Please fill in all the fields.');
+      return;
+    }
     setLoading(true);
     const pitch = `Most people have this problem: ${problem}\nWe have this solution: ${solution}\nSo that we have a happy ending: ${happyEnding}`;
     const response = await axios.post('/api/evaluate', { pitch });
@@ -24,7 +30,7 @@ const Evaluate: React.FC = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+    <div className="max-w-2xl mx-auto py-16 px-8 sm:py-24 sm:px-12 lg:px-16">
       <h1 className="text-2xl font-semibold text-center mb-6">
         Pitch Evaluator. Enter your pitch in 3 parts.
       </h1>
@@ -51,24 +57,28 @@ const Evaluate: React.FC = () => {
           className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
         ></textarea>
         {loading && (
-          <div className="h-2 w-full bg-blue-200 mb-4">
-            <div className="h-full w-1/2 bg-blue-500 animate-ping"></div>
+          <div className="flex items-center justify-center mb-4">
+            <div className="w-5 h-5 border-t-2 border-blue-500 border-solid rounded-full animate-spin"></div>
           </div>
         )}
-        <button
-          type="submit"
-          disabled={loading}
-          className={`w-full py-3 px-4 text-white ${
-            loading ? 'bg-blue-400' : 'bg-blue-500 hover:bg-blue-600'
-          } rounded-lg focus:outline-none focus:shadow-outline`}
-        >
-          Evaluate
-        </button>
+        {!loading && (
+          <button
+            type="submit"
+            disabled={loading}
+            className={`w-full py-3 px-4 text-white ${
+              loading ? 'bg-blue-400' : 'bg-blue-500 hover:bg-blue-600'
+            } rounded-lg focus:outline-none focus:shadow-outline`}
+          >
+            Evaluate
+          </button>
+        )}
       </form>
+      {error && (
+        <div className="mt-4 text-red-500 font-semibold">{error}</div>
+      )}
       {evaluation && (
         <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4">Evaluation:</h2>
-          <p className="text-gray-700">{evaluation}</p>
+          {evaluation}
         </div>
       )}
     </div>
